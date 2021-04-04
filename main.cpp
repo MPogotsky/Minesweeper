21266f84e9745b6e61df21b4e9c2d8b26ef76057
Matsvei Pahotski <matsvei.pahotski@gmail.com>
2021-04-04 22:52:38 +0200

    debug_display modified

--- main.cpp
+++ main.cpp
@@ -2,17 +2,12 @@
 #include "MinesweeperBoard/MinesweeperBoard.h"
 
 void debug_display(MinesweeperBoard &mainBoard) {
-  for(int height = 0; height < mainBoard.getBoardHeight(); height++){
-    for(int width = 0; width < mainBoard.getBoardWidth(); width++){
-      std::cout << "[...]";
-    }
-    std::cout << '\n';
-  }
+  mainBoard.printRawBoard();
 }
 
 int main(int argc, char const *argv[])
 {
-  MinesweeperBoard mainBoard(10, 10);
+  MinesweeperBoard mainBoard(4, 4);
   debug_display(mainBoard);
   return 0;
 }
